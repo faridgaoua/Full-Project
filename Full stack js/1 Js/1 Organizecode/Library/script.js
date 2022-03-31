518364316ef57cbefbaf52c527b77e2ed2176d6e
Farid Gaoua <faridgaoua8@gmail.com>
2022-03-31 15:41:34 +0100

    change structure library project

--- Full stack js/1 Js/1 Organizecode/Library/script.js
+++ Full stack js/1 Js/1 Organizecode/Library/script.js
@@ -4,20 +4,25 @@ function Book(user) {
   this.user=user;
 }
 //Add Book
-document.getElementById('button').addEventListener('click', () => {
- let userInput= document.getElementById('user');
+function addBookToLibrary() {
+  // do stuff here
+  let userInput= document.getElementById('user');
  let book = new Book(userInput.value);
  const div= document.getElementById('bookdis');
  myLibrary.push(book);
  div.innerHTML += `<div>${book.user} <span id="x" class="x">+</span><div>`;
  userInput.value=''
-})
-//Delete Book
-document.querySelector('#bookdis').addEventListener('click', (e) => {
- if (e.target.classList.contains('x')) {
-e.target.parentElement.remove();
 }
-})
+
+//Delete Book
+function removeBookFromLibrary(e) {
+  if (e.target.classList.contains('x')) {
+    e.target.parentElement.remove();
+    }}
+
+
+document.getElementById('button').addEventListener('click', addBookToLibrary);
+document.querySelector('#bookdis').addEventListener('click', (e) => removeBookFromLibrary(e))
 
 
 
